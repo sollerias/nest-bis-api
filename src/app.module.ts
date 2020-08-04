54269d80f773306b747bd22462a26bfc92a128e3
Gleb Tregubov <gleb260791@gmail.com>
2020-08-04 10:29:35 +0200

    add directories for wallet, transaction, coin

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { WalletModule } from './wallet/wallet.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { CoinModule } from './coin/coin.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule
+    AuthModule,
+    WalletModule,
+    TransactionModule,
+    CoinModule
   ],
 })
 
